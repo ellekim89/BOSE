fb514bbdfde1047897515d916002c1b4f45ff89d
ellekim89 <ellekim89@gmail.com>
2015-08-17 14:31:49 -0700

    routes

--- GoodrHood/index.js
+++ GoodrHood/index.js
@@ -6,6 +6,9 @@ var methodOverride = require('method-override');
 var ejsLayouts = require('express-ejs-layouts');
 var session = require('express-session');
 var flash = require('connect-flash');
+var Zillow  = require('node-zillow')
+var zwsid = process.env.ZILLOW_KEY
+var zillow = new Zillow(zwsid)
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -20,8 +23,80 @@ app.use(session({
 }));
 app.use(flash());
 
+app.get("/signup", function(req,res){
+  // TODO: ENTER CODE HERE
+  res.render('main/signup');
+});
+
+app.post("/signup", function(req, res){
+    // TODO: ENTER CODE HERE
+})
+app.get("/login", function(req,res){
+    // TODO: ENTER CODE HERE
+    res.render('main/login');
+});
+
+app.get("/favorites", function(req,res){
+    // TODO: ENTER CODE HERE
+    res.render('main/favorites');
+})
+
 app.get("/", function(req, res) {
+
+
     res.render("main/index")
+
+// ELLE'S ZILLOW NOTES!
+//   var params = {
+//   address: "106 NW 42nd St",
+//   city: 'Seattle',
+//   state: 'WA',
+//   zip: '98107'
+// }
+
+
+//  var parameters = {
+//     zpid: 48998252
+//   };
+//   //getting images ect.
+//   zillow.callApi('GetUpdatedPropertyDetails', parameters)
+//     .then(function(data) {
+//       //var results = data.response[0].results[0].result[0]
+//      console.log(data.response[0])
+//      });
+
+// app.get("/", function(req, res) {
+//   var parameters = {
+//     zpid: 48998252
+//   };
+//   //getting images ect.
+//   zillow.callApi('GetUpdatedPropertyDetails', parameters)
+//     .then(function(data) {
+//       //var results = data.response[0].results[0].result[0]
+//      console.log(data.response[0])
+//     })
+// });
+
+
+// getting zpid
+// getDeepSearchResults() returns zpid, which is passed into getUpdatedPropertyDetails()
+// zillow.getDeepSearchResults(params)
+//     .then(function(result) {
+//       var zpid = result.response[0].results[0].result[0].zpid[0]
+//       //console.log(result.response[0].results[0].result[0].zestimate[0])
+//       console.log(zillow.getUpdatedPropertyDetails(zpid))
+//     })
+//     .then(function(result) {
+//       if (result.message[0].code === '502') return {}
+//       console.log(result.response[0])
+//     })
+
+});
+
+
+app.get("/:id/results", function(req, res){
+  // TODO: ENTER CODE HERE
+  res.render('main/results')
 });
 
 
